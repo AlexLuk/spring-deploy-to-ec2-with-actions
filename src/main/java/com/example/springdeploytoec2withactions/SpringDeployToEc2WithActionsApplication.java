5a19d37877dc08181d731e835412258ee23c3b6f
Lukovkin, Aleksandr [External] <aleksandr.lukovkin@externals.adidas.com>
2023-03-17 16:26:44 +0300

    project init

---
+++ src/main/java/com/example/springdeploytoec2withactions/SpringDeployToEc2WithActionsApplication.java
@@ -0,0 +1,13 @@
+package com.example.springdeploytoec2withactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDeployToEc2WithActionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDeployToEc2WithActionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springdeploytoec2withactions/SpringDeployToEc2WithActionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springdeploytoec2withactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDeployToEc2WithActionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
